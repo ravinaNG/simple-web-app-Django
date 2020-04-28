ddd4a456d7257f8aaa0fb0f5a7960a06953b5ac1
Ravina <40683703+ravinaNG@users.noreply.github.com>
2020-04-28 15:59:09 +0530

    tring to rrender with HTML page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,12 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You are in the polls index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You are in the polls index.")
+
+def geeks_view(request): 
+    # render function takes argument  - request 
+    # and return HTML as response 
+    return render(request, "home.html") 
+
 
